b64584e332b85440840c4f0372a59c8add8cead8
MohsinA-bit <56942181+MohsinA-bit@users.noreply.github.com>
2019-11-06 19:42:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week_1_Dan_Witherspoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This class is awesome
+            //This program multiplies 2 numbers
+
+            string str1, str2;
+
+            str1 = "hello world";
+            str2 = "Dan Witherspoon";
+
+
+            Console.WriteLine(str1 + " from " + str2);
+            
+        }
+    }
+}
